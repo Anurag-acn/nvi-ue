2cae38d7a88f5fa3a4891612057a6027789be20d
Mukta Sethi <mukta.sethi@dsm.com>
2025-04-14 17:18:02 +0530

    reverted cards js to test

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -2,50 +2,23 @@ import { createOptimizedPicture } from '../../scripts/aem.js';
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
+  /* change to ul, li */
   const ul = document.createElement('ul');
-  ul.classList.add('cards-list');
-
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
-    li.classList.add('cards-card');
-
-    // Move instrumentation
     moveInstrumentation(row, li);
-
-    const rowDivs = [...row.children];
-    rowDivs.forEach((col, i) => {
-      const div = document.createElement('div');
-
-      // Detect image column
-      if (col.querySelector('picture')) {
-        div.className = 'cards-card-image';
-        const img = col.querySelector('img');
-        const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
-        moveInstrumentation(img, optimizedPic.querySelector('img'));
-        div.append(optimizedPic);
-      } else {
-        div.className = 'cards-card-body';
-        const paras = col.querySelectorAll('p');
-        paras.forEach((p, index) => {
-          if (index === 0) p.classList.add('card-title');
-          else if (index === 1) p.classList.add('card-subtitle');
-          else if (p.querySelector('a')) {
-            p.classList.add('card-link');
-            const links = p.querySelectorAll('a');
-            links.forEach((a, i) => a.classList.add(`card-link${i + 1}`));
-          } else {
-            p.classList.add('card-description');
-          }
-          div.append(p);
-        });
-      }
-
-      li.append(div);
+    while (row.firstElementChild) li.append(row.firstElementChild);
+    [...li.children].forEach((div) => {
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+      else div.className = 'cards-card-body';
     });
-
     ul.append(li);
   });
-
+  ul.querySelectorAll('picture > img').forEach((img) => {
+    const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
+    moveInstrumentation(img, optimizedPic.querySelector('img'));
+    img.closest('picture').replaceWith(optimizedPic);
+  });
   block.textContent = '';
   block.append(ul);
 }
