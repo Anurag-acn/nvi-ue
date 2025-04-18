8deda2386f71a7ea2f751365c11ea8d263130954
Soundrapandian <k.soundrapandian@accenture.com>
2025-04-18 16:45:53 +0530

    Hero changes

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,41 +1,51 @@
 export default function decorate(block) {
-    const counts = {
-      image: 0,
-      cta: 0,
-      text: 0,
-      other: 0,
-    };
-  
-    const ctaElements = [];
-  
-    [...block.children].forEach((div) => {
-      if (div.querySelector('picture')) {
-        counts.image += 1;
-        div.classList.add(`hero-image-${counts.image}`);
-      } else if (div.querySelector('a.button')) {
-        counts.cta += 1;
-        div.classList.add(`hero-cta-${counts.cta}`);
-        ctaElements.push(div); // collect the CTA element
-      } else if (div.querySelector('p')) {
-        counts.text += 1;
-        div.classList.add(`hero-text-${counts.text}`);
-      } else {
-        counts.other += 1;
-        div.classList.add(`hero-section-${counts.other}`);
-      }
-    });
-  
-    // If there are any CTA elements, wrap them in a container
-    if (ctaElements.length > 0) {
-      const ctaContainer = document.createElement('div');
-      ctaContainer.classList.add('hero-cta-container');
-  
-      ctaElements.forEach((cta) => {
-        ctaContainer.appendChild(cta);
-      });
-  
-      // Append the container to the block
-      block.appendChild(ctaContainer);
+  const counts = {
+    image: 0,
+    cta: 0,
+    text: 0,
+    other: 0,
+  };
+
+  let imageDiv;
+
+  [...block.children].forEach((div) => {
+    if (div.querySelector('picture')) {
+      counts.image += 1;
+      div.classList.add(`hero-image-${counts.image}`);
+      imageDiv = div;
     }
+  });
+
+  const contentWrapper = document.createElement('div');
+  contentWrapper.classList.add('hero-content-wrapper');
+
+  const wrapperDiv = imageDiv?.nextElementSibling;
+  const firstInnerDiv = wrapperDiv?.querySelector('div');
+  const contentElements = firstInnerDiv ? [...firstInnerDiv.children] : [];
+  const ctaContainer = document.createElement('div');
+  ctaContainer.classList.add('hero-cta-container');
+
+  contentElements.forEach((el) => {
+    if (el.querySelector('a.button')) {
+      counts.cta += 1;
+      const ctaDiv = document.createElement('div');
+      ctaDiv.classList.add(`hero-cta-${counts.cta}`);
+      ctaDiv.appendChild(el);
+      ctaContainer.appendChild(ctaDiv);
+    } else {
+      counts.text += 1;
+      const textDiv = document.createElement('div');
+      textDiv.classList.add(`hero-text-${counts.text}`);
+      textDiv.appendChild(el);
+      contentWrapper.appendChild(textDiv);
+    }
+  });
+
+  if (ctaContainer.children.length > 0) {
+    contentWrapper.appendChild(ctaContainer);
   }
-  
+
+  imageDiv?.nextElementSibling?.remove();
+
+  block.appendChild(contentWrapper);
+}
